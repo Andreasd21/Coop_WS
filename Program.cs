364f6e367dc4ad9a42ab28b2c5a7289de575315b
Andreas <89839661+Andreasd21@users.noreply.github.com>
2025-01-17 22:54:53 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,8 +7,17 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSignalR();
 
-// Add CORS services
-builder.Services.AddCors();
+// Add CORS services and define a named policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowedOriginsPolicy", builder =>
+    {
+        builder.WithOrigins("https://coopfront-674574933021.europe-central2.run.app/") // Replace with your frontend URL
+               .AllowAnyHeader()
+               .AllowAnyMethod()
+               .AllowCredentials(); // Allow credentials if needed
+    });
+});
 
 var app = builder.Build();
 
@@ -19,12 +28,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-// Use the configured CORS policy
-app.UseCors(builder =>
-    builder.AllowAnyHeader()
-           .AllowAnyMethod()
-           .SetIsOriginAllowed((host) => true)
-           .AllowCredentials());
+// Use the named CORS policy
+app.UseCors("AllowedOriginsPolicy");
 
 app.UseHttpsRedirection();
 app.MapHub<ChatHub>("/signalr");
