4a296dbbe98916f80696566bcc3d2d9264b8beed
Andreas <89839661+Andreasd21@users.noreply.github.com>
2025-01-19 16:44:19 +0100

    Update CanvasHub.cs

--- Hubs/CanvasHub.cs
+++ Hubs/CanvasHub.cs
@@ -3,24 +3,24 @@ using Newtonsoft.Json;
 using StackExchange.Redis;
 using Google.Cloud.PubSub.V1;
 using System.Threading.Tasks;
-using CooP_WS.Modal;
 
 namespace CooP_WS.Hubs
 {
     public class CanvasHub : Hub
     {
         private readonly PublisherClient _publisherClient;
+        private readonly ISubscriber _redisSubscriber;
         private const string TopicId = "Changes";
 
-        public CanvasHub()
+        public CanvasHub(IConnectionMultiplexer redis)
         {
             var projectId = "coop-443623";
             var topicName = TopicName.FromProjectTopic(projectId, TopicId);
 
             _publisherClient = PublisherClient.Create(topicName);
+            _redisSubscriber = redis.GetSubscriber();
         }
 
-       
         public async Task UpdatePixel(int x, int y, string color)
         {
             var messagePayload = new
@@ -29,9 +29,17 @@ namespace CooP_WS.Hubs
                 y = y,
                 color = color
             };
+
+            // Serialize the message payload
             var messageJson = JsonConvert.SerializeObject(messagePayload);
 
+            // Publish to Google Pub/Sub
             await _publisherClient.PublishAsync(messageJson);
+
+            // Publish to Redis for SignalR clients
+            await _redisSubscriber.PublishAsync("PixelUpdates", messageJson);
+
+            Console.WriteLine($"Pixel updated: x={x}, y={y}, color={color}");
         }
     }
 }
